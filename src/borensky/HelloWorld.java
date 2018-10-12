5809f2624fdbfe014d457c2d03a507c85db582a0
jborensky <jborensky@student.tgm.ac.at>
2018-10-12 12:07:58 +0200

    3 Hello World

--- src/borensky/HelloWorld.java
+++ src/borensky/HelloWorld.java
@@ -3,5 +3,7 @@ package borensky;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
 	}
 }
